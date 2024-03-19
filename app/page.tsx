d63a1cf1433ae21b38ac494d61bd121843960c44
Alex <alex.pober.95@gmail.com>
2024-03-18 17:16:58 -0700

    new landing page girl

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,26 @@ import { Flex, Text, Button } from '@radix-ui/themes';
 import NavBar from "@/components/NavBar";
 import FourPillars from "@/components/FourtPillars";
 import WelcomeToTOA from "@/components/WelcomeToTOA";
+import LandingGirl from "@/public/TOA-lading-image.png"
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center max-w-screen-lg m-auto">
       <NavBar />
-      <div className="max-w-screen-md w-full px-3">
+      <div className="max-w-screen-md w-full mt-8 px-3">
         <h1 className="text-bungee text-5xl w-full text-toa-3">
           An Agency By Women
         </h1>
         <h1 className="text-bungee text-5xl text-toa-4 mr-[2.25rem] w-full flex flex-row-reverse ">
           For Woman
         </h1>
+        <Image
+          src={LandingGirl}
+          alt="Girl laying on couch image"
+          width={500}
+          height={500}
+          className="m-auto"
+        />
       </div>
       <WelcomeToTOA />
       <FourPillars />
--- components/WelcomeToTOA.tsx
+++ components/WelcomeToTOA.tsx
@@ -7,12 +7,12 @@ import CreativtyIcon from "@/public/creativity-innovation-icon.png";
 import GrowthIcon from "@/public/strategic-growth-icon.png";
 export default function WelcomeToTOA() {
   return (
-    <div className="w-screen bg-toa-5 my-5">
+    <div className="w-screen bg-toa-4 my-5">
       <div className="flex flex-wrap p-9 bg-transparent gap-8 max-w-screen-md m-auto justify-center">
         <h1 className="text-bungee text-toa-1 text-2xl font-bold max-w-[300px]">
           Welcome to The Only Advisors
         </h1>
-        <p className="leading-6 font-semibold text-toa-3 max-w-xs">
+        <p className="leading-5 font-semibold text-toa-1 max-w-xs">
           The forefront of AI innovation. At TOA, we transcend traditional social
           media management, pioneering a transformative approach for OnlyFans
           creators to amplify their reach. Our core mission? To empower creators
