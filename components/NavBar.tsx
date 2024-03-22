bd752a5be0aa8cdbdee2529e81fffa980eb976f6
Alex <alex.pober.95@gmail.com>
2024-03-22 11:09:04 -0700

    new 2D logo

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,11 +2,12 @@ import Logo from "@/public/logo.png";
 import Image from "next/image";
 import Link from "next/link";
 import NavBarButton from "./NavBarButton";
+import logo2d from '@/public/logo2D.svg'
 export default function NavBar() {
   return (
     <div className="flex max-w-screen-md items-center m-auto justify-around p-3 bg-transparent gap-12 h-16">
       <Link href="/">
-        <Image alt="logo image" src={Logo} width={100} height={60} />
+        <Image alt="logo image" src={logo2d} width={100} height={60} />
       </Link>
 
       <div className="flex">
